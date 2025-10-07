83a93aa16b051d0231677797a25104b6ae96795a
John Grey <johngrey4296@gmail.com>
2025-10-07 03:17:19 +0100

    [add]: initial working

--- lib/utils.bash
+++ lib/utils.bash
@@ -37,8 +37,7 @@ function sort_versions() {
 
 function list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//'
+		grep -o 'refs/tags/releases/.*' | cut -d/ -f4- | sed 's/^v//'
 	# NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
@@ -54,13 +53,14 @@ function download_release() {
 	url="$GH_REPO/releases/download/releases/${version}/${filename}"
 
 	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	mkdir -p "$ASDF_DOWNLOAD_PATH"
+	curl "${curl_opts[@]}" -o "$ASDF_DOWNLOAD_PATH/$filename" -C - "$url" || fail "Could not download $url"
 }
 
 function install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -68,11 +68,11 @@ function install_version() {
 
 	(
 		mkdir -p "$install_path"
-		"$ASDF_DOWNLOAD_PATH/$TOOL_NAME-$ASDF_INSTALL_VERSION.sh" --install-dir "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH/SoarSuite_${ASDF_INSTALL_VERSION}-Multiplatform"/* "$install_path"
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		# test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
